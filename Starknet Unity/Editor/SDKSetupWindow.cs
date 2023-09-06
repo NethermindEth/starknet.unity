e8a8163e3e117ab7510788dda118d2858a8a0c89
Jelilat Anofiu <anofiujelailah@gmail.com>
2023-09-06 14:33:19 +0100

    add dojo support

--- Starknet Unity/Editor/SDKSetupWindow.cs
+++ Starknet Unity/Editor/SDKSetupWindow.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class SDKSetupWindow : EditorWindow
 {
     string rpcNode = "Enter RPC Node";
+    private string[] options = new string[] { "Dojo", "Starknet" }; // Add or remove options as needed.
+    private int selectedIndex = 0;
+    private string worldAddress = "Enter World Address";
 
     [MenuItem("Starknet SDK/Setup")]
     public static void ShowWindow()
@@ -14,8 +17,14 @@ public class SDKSetupWindow : EditorWindow
     void OnGUI()
     {
         GUILayout.Label("Setup your Starknet SDK", EditorStyles.boldLabel);
+        selectedIndex = EditorGUILayout.Popup("Game Engine", selectedIndex, options);
 
         rpcNode = EditorGUILayout.TextField("RPC Node", rpcNode);
+
+        if (selectedIndex == 0)
+        {
+            EditorGUILayout.TextField("World address", worldAddress);
+        }
 
         if (GUILayout.Button("Submit"))
         {
@@ -28,5 +37,7 @@ public class SDKSetupWindow : EditorWindow
     void SetupSDK(string rpc)
     {
         PlayerPrefs.SetString("RPCNode", rpc);
+        PlayerPrefs.SetString("Game Engine", options[selectedIndex]);
+        PlayerPrefs.SetString("World Address", worldAddress);
     }
 }
